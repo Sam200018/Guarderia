2f1b67dbda06a088d701057ca780bbb86293d23a
Samuel Bautista <samuelbaubas@gmail.com>
2023-03-02 15:46:26 -0600

    Refactoring login_screen.kt and login_viewmodel.kt

--- app/src/main/java/com/example/guarderia/domain/viewmodel/login_viewmodel.kt
+++ app/src/main/java/com/example/guarderia/domain/viewmodel/login_viewmodel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 
 class LoginViewModel : ViewModel() {
     private val _email = MutableLiveData<String>()
-    val email: LiveData<String> = _email;
+    val email: LiveData<String> = _email
 
     private val _password = MutableLiveData<String>()
     val password: LiveData<String> = _password
