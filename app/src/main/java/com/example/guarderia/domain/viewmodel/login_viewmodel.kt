b7d50ccc1778d8abd205e0d846d644592df432e7
Samuel Bautista <samuelbaubas@gmail.com>
2023-03-02 20:02:14 -0600

    Refactoring values in viewmodel

--- app/src/main/java/com/example/guarderia/domain/viewmodel/login_viewmodel.kt
+++ app/src/main/java/com/example/guarderia/domain/viewmodel/login_viewmodel.kt
@@ -4,12 +4,14 @@ import android.util.Patterns
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.navigation.NavHostController
+import com.example.guarderia.ui.routes.Routes
 
-class LoginViewModel : ViewModel() {
-    private val _email = MutableLiveData<String>()
+class LoginViewModel(private val navigator: NavHostController) : ViewModel() {
+    private val _email = MutableLiveData("")
     val email: LiveData<String> = _email
 
-    private val _password = MutableLiveData<String>()
+    private val _password = MutableLiveData("")
     val password: LiveData<String> = _password
 
     private val _isLoginEnable = MutableLiveData<Boolean>()
@@ -17,19 +19,20 @@ class LoginViewModel : ViewModel() {
 
     fun onLoginChange(email: String) {
         _email.value = email
-        _isLoginEnable.value=enableLogin(email,_password.value!!)
+        _isLoginEnable.value = enableLogin(email, _password.value!!)
     }
 
 
     fun onPasswordChange(password: String) {
         _password.value = password
-        _isLoginEnable.value=enableLogin(_email.value!!,password)
+        _isLoginEnable.value = enableLogin(_email.value!!, password)
     }
 
     private fun enableLogin(email: String, password: String) =
         Patterns.EMAIL_ADDRESS.matcher(email).matches() && password.length > 6
 
-     fun login(){
+    fun login() {
+        navigator.navigate(Routes.ChildrenScreen.route)
         println("Login")
     }
 }
