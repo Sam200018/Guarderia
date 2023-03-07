254bfe2c43efc51ba1d60239aaa86e8056602ec3
Samuel Bautista <samuelbaubas@gmail.com>
2023-03-07 13:57:07 -0600

    Add Report model

---
+++ app/src/main/java/com/example/guarderia/domain/entities/Report.kt
@@ -0,0 +1,14 @@
+package com.example.guarderia.domain.entities
+
+import java.util.*
+
+data class Report(
+    val isEditable: Boolean,
+    val foodRecord: ArrayList<Food>,
+    val evacuationRecord: ArrayList<Evacuation>,
+    var detailsRecord: String,
+)
+data class Evacuation(var evacution: String, var date: Date)
+
+data class Food(var food: String, var status: String)
+
