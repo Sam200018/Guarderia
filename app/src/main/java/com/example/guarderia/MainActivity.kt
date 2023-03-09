028c1cc2ba6d9523305967638c4194d7a683475d
C. H. Pelayo <pelayohe94@gmail.com>
2023-03-09 15:18:56 -0600

    Add reportParentViewModel: create variable, using it as a shared viewmodel.

--- app/src/main/java/com/example/guarderia/MainActivity.kt
+++ app/src/main/java/com/example/guarderia/MainActivity.kt
@@ -7,17 +7,18 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.guarderia.domain.viewmodel.ChildrenViewModel
 import com.example.guarderia.domain.viewmodel.LoginViewModel
 import com.example.guarderia.domain.viewmodel.ReportCarerViewModel
+import com.example.guarderia.domain.viewmodel.ReportParentViewModel
 import com.example.guarderia.ui.routes.Routes
 import com.example.guarderia.ui.screens.ChildrenScreen
 import com.example.guarderia.ui.screens.LoginScreen
 import com.example.guarderia.ui.screens.ReportCarerScreen
+import com.example.guarderia.ui.screens.ReportParentScreen
 import com.example.guarderia.ui.theme.GuarderiaTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,9 +34,11 @@ class MainActivity : ComponentActivity() {
                 ) {
 
                     val navigator = rememberNavController()
-
                     val reportCarerViewModel= ReportCarerViewModel(navigator)
+                    val reportParentViewModel = ReportParentViewModel(navigator)
+
                     NavHost(navController = navigator, startDestination = Routes.LoginScreen.route) {
+
                         composable(Routes.LoginScreen.route) { LoginScreen(LoginViewModel(navigator)) }
 
                         composable("${Routes.ChildrenScreen.route}/{userEmail}/{type}") {
@@ -43,14 +46,18 @@ class MainActivity : ComponentActivity() {
                             val type = it.arguments?.getString("type")
 
                             if (userEmail != null && type != null) {
-                                ChildrenScreen(ChildrenViewModel(navigator),reportCarerViewModel, userEmail, type)
+                                ChildrenScreen(ChildrenViewModel(navigator),reportCarerViewModel, reportParentViewModel, userEmail, type)
                             }
                         }
 
                         composable(Routes.ReportCarerScreen.route) {
-
                             ReportCarerScreen(reportCarerViewModel,navigator)
                         }
+
+                        composable(Routes.ReportParentScreen.route){
+                            ReportParentScreen(reportParentViewModel, navigator)
+                        }
+
                     }
 
                 }
