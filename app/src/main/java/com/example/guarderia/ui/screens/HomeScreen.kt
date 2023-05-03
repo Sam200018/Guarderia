e3604dc49e5317c6d185268e6f06ee238200a91a
Samuel Bautista <samuelbaubas@gmail.com>
2023-05-03 16:20:37 -0600

    HomeScreen with harcode text

--- app/src/main/java/com/example/guarderia/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/guarderia/ui/screens/HomeScreen.kt
@@ -1,7 +1,9 @@
 package com.example.guarderia.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
@@ -9,7 +11,8 @@ import androidx.compose.ui.Modifier
 fun HomeScreen(
     modifier: Modifier = Modifier
 ) {
-    Column(modifier= modifier.fillMaxSize()) {
+    Column(modifier = modifier.fillMaxSize(), verticalArrangement = Arrangement.Center) {
 //        TODO: implementar la pantalla completa
+        Text(text = "Hola mundo")
     }
 }
