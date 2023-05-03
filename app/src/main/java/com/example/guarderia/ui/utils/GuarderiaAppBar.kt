7bbbccd9e0f5fffa1e58d56bc0b2b27b2ee7bb6f
Samuel Bautista <samuelbaubas@gmail.com>
2023-05-03 16:17:28 -0600

    top bar component to use it in all app

---
+++ app/src/main/java/com/example/guarderia/ui/utils/GuarderiaAppBar.kt
@@ -0,0 +1,34 @@
+package com.example.guarderia.ui.utils
+
+
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.guarderia.ui.routes.GuarderiaRoutes
+
+@Composable
+fun GuarderiaAppBar(
+    currentRoute: GuarderiaRoutes,
+    canNavigateBack: Boolean,
+    navigateUp: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    TopAppBar(title = { Text(text = currentRoute.name) },
+        modifier = modifier,
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = "back"
+                    )
+                }
+            }
+        }
+    )
+}
