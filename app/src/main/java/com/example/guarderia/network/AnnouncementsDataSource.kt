f53f9d4ce68ae904faf0ab154001672237134363
Samuel Bautista <samuelbaubas@gmail.com>
2023-06-23 17:22:14 -0600

    Update announcement data source

--- app/src/main/java/com/example/guarderia/network/AnnouncementsDataSource.kt
+++ app/src/main/java/com/example/guarderia/network/AnnouncementsDataSource.kt
@@ -1,11 +1,20 @@
 package com.example.guarderia.network
 
+import com.example.guarderia.model.Announcement
+import com.example.guarderia.model.AnnouncementResponse
+import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.POST
 
 interface AnnouncementsDataSourceRemote {
 
-    @GET("/AllNotices")
-    suspend fun getNoticeById(@Header("Authorization")token: String)
+    @GET("allNotices")
+    suspend fun getNoticeById(@Header("Authorization")token: String): AnnouncementResponse
+
+    @POST("createNotice")
+    suspend fun createNotice(
+        @Header("Authorization")token: String,
+        @Body announcement: Announcement)
 
 }
