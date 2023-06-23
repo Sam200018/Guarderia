153fc73c286de2c8a9c74dd4d1c57d560b8286e4
Samuel Bautista <samuelbaubas@gmail.com>
2023-06-23 17:33:50 -0600

    Update GuarderiaAppBar to show logout icon in specific screens

--- app/src/main/java/com/example/guarderia/ui/utils/GuarderiaAppBar.kt
+++ app/src/main/java/com/example/guarderia/ui/utils/GuarderiaAppBar.kt
@@ -7,17 +7,22 @@ import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Logout
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import com.example.guarderia.domain.viewmodel.auth.AuthViewModel
 import com.example.guarderia.ui.routes.GuarderiaRoutes
+import kotlinx.coroutines.runBlocking
 
 @Composable
 fun GuarderiaAppBar(
+    modifier: Modifier = Modifier,
     currentRoute: GuarderiaRoutes,
     canNavigateBack: Boolean,
     navigateUp: () -> Unit,
-    modifier: Modifier = Modifier
+    authViewModel: AuthViewModel,
+    isLogoutEnable: Boolean
 ) {
     TopAppBar(title = { Text(text = stringResource(id = currentRoute.title)) },
         modifier = modifier,
@@ -28,6 +33,17 @@ fun GuarderiaAppBar(
                         imageVector = Icons.Filled.ArrowBack,
                         contentDescription = "back"
                     )
+                }
+            }
+        },
+        actions = {
+            if (isLogoutEnable) {
+                IconButton(onClick = {
+                    runBlocking {
+                        authViewModel.logout(null)
+                    }
+                }) {
+                    Icon(Icons.Filled.Logout, contentDescription = "")
                 }
             }
         }
