3521169dd7945d76a0919ea856b7d4d147462aba
Samuel Bautista <samuelbaubas@gmail.com>
2023-06-23 17:29:38 -0600

    Add Announcement data class to map the data from the server

---
+++ app/src/main/java/com/example/guarderia/model/Announcement.kt
@@ -0,0 +1,16 @@
+package com.example.guarderia.model
+
+import com.google.gson.annotations.SerializedName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Announcement (
+    val id: Int= 1,
+
+    @SerializedName("importance") val importance: Int,
+    @SerializedName("date") val date: String,
+    @SerializedName("title") val title: String,
+    @SerializedName("body") val body: String,
+    @SerializedName("id_group") val idGroup: Long
+)
+
