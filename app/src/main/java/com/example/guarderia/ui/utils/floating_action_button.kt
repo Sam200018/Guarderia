cdfd45f9b0db8788f0704e9309ec5ee4e78437e6
Samuel Bautista <samuelbaubas@gmail.com>
2023-06-23 17:33:16 -0600

    Update floating_action_button.kt to only use it when is necessary

--- app/src/main/java/com/example/guarderia/ui/utils/floating_action_button.kt
+++ app/src/main/java/com/example/guarderia/ui/utils/floating_action_button.kt
@@ -6,13 +6,19 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.colorResource
+import androidx.navigation.NavHostController
 import com.example.guarderia.R
+import com.example.guarderia.ui.routes.GuarderiaRoutes
 
 @Composable
-fun GuarderiaFloatingActionButton() {
+fun GuarderiaFloatingActionButton(navController: NavHostController, nextRoute: String) {
     FloatingActionButton(onClick = {
-
+        val nextScreen = when (nextRoute) {
+            "Home" -> GuarderiaRoutes.AddNotice
+            else -> GuarderiaRoutes.Home
+        }
+        navController.navigate(nextScreen.name)
     }, backgroundColor = colorResource(id = R.color.purple_700)) {
-        Icon(Icons.Filled.Add,contentDescription = "")
+        Icon(Icons.Filled.Add, contentDescription = "")
     }
 }
