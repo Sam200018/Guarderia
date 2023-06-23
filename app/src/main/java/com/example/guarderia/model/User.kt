08a34585bb15019f588207ca6f12713f03a6db27
Samuel Bautista <samuelbaubas@gmail.com>
2023-06-22 21:26:17 -0600

    update user data class to make it an entity to save it in local

--- app/src/main/java/com/example/guarderia/model/User.kt
+++ app/src/main/java/com/example/guarderia/model/User.kt
@@ -1,10 +1,14 @@
 package com.example.guarderia.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
+@Entity
 @Serializable
 data class User(
+    @PrimaryKey
     val id: Int,
     val name: String,
     val email: String,
