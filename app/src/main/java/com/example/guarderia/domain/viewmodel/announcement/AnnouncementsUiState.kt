fa4cf84225950610a955631d27a1249f7e51a010
Samuel Bautista <samuelbaubas@gmail.com>
2023-06-26 17:24:33 -0600

    Add validation to announcements publish form

---
+++ app/src/main/java/com/example/guarderia/domain/viewmodel/announcement/AnnouncementsUiState.kt
@@ -0,0 +1,6 @@
+package com.example.guarderia.domain.viewmodel.announcement
+
+data class AnnouncementsUiState(
+    val isTitleValid: Boolean = true,
+    val isImportanceSelected: Boolean = true,
+)
