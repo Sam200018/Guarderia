432524094966d84e42a100293f12d27899c42d9c
Samuel Bautista <samuelbaubas@gmail.com>
2023-06-26 11:44:48 -0600

    Update GuarderiaBottomNav

--- app/src/main/java/com/example/guarderia/ui/utils/GuarderiaBottomNav.kt
+++ app/src/main/java/com/example/guarderia/ui/utils/GuarderiaBottomNav.kt
@@ -13,13 +13,13 @@ import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.example.guarderia.domain.viewmodel.bottomNav.BottomNavViewModel
-import com.example.guarderia.ui.routes.GuarderiaRoutes
+import com.example.guarderia.ui.routes.Routes
 import com.example.guarderia.ui.theme.FilterButtonDisableColor
 
 //TODO: Add all the titles of the bottom bar
 val items = listOf(
-    GuarderiaRoutes.Home,
-    GuarderiaRoutes.Notes
+    Routes.Home,
+    Routes.Notes
 )
 
 @Composable
@@ -38,8 +38,8 @@ fun GuarderiaBottomNav(
                     val previousScreen= items[bottomNavUiState]
                     bottomNavViewModel.onSelectedRouteChange(index)
                     if (index != bottomNavUiState) {
-                        navController.navigate(item.name){
-                            popUpTo(previousScreen.name) {
+                        navController.navigate(item.route){
+                            popUpTo(previousScreen.route) {
                                 inclusive = true
                             }
                         }
