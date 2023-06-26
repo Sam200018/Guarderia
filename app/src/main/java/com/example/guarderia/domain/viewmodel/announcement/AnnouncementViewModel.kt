31c38547f843672be6a615d07640a7d44e2b0388
Samuel Bautista <samuelbaubas@gmail.com>
2023-06-26 11:36:32 -0600

    Update announcement vm

--- app/src/main/java/com/example/guarderia/domain/viewmodel/announcement/AnnouncementViewModel.kt
+++ app/src/main/java/com/example/guarderia/domain/viewmodel/announcement/AnnouncementViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.guarderia.domain.viewmodel.announcement
 
+import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
@@ -11,6 +12,9 @@ import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.guarderia.GuarderiaApplication
 import com.example.guarderia.data.AnnouncementsRepository
 import com.example.guarderia.model.Announcement
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class AnnouncementViewModel(
     private val announcementsRepository: AnnouncementsRepository
@@ -22,7 +26,7 @@ class AnnouncementViewModel(
     var importanceInput: Int by mutableStateOf(0)
         private set
 
-    var dueDateInput: String by mutableStateOf("")
+    var dueDateInput: Date by mutableStateOf(Date())
         private set
 
     var descriptionInput: String by mutableStateOf("")
@@ -36,7 +40,7 @@ class AnnouncementViewModel(
         importanceInput = importance
     }
 
-    fun onDueDateChange(dueDate: String) {
+    fun onDueDateChange(dueDate: Date) {
         dueDateInput = dueDate
     }
 
@@ -47,17 +51,23 @@ class AnnouncementViewModel(
 
     suspend fun sendAnnouncement() {
 
-            val user = announcementsRepository.getUser()
-            val tokenEntity = announcementsRepository.getToken()
+        val tokenEntity = announcementsRepository.getToken()
 //        TODO: get id group
-            val announcementToSend = Announcement(
-                importance = importanceInput,
-                date = "2023-08-03",
-                title = titleInput,
-                body = descriptionInput,
-                idGroup = 1
-            )
-            announcementsRepository.sendAnnouncement(tokenEntity.token, announcementToSend)
+
+        val announcementToSend = Announcement(
+            importance = importanceInput,
+            date = SimpleDateFormat("yyyy/MM/dd", Locale.getDefault()).format(dueDateInput),
+            title = titleInput,
+            body = descriptionInput,
+        )
+        try {
+            if (tokenEntity != null) {
+                announcementsRepository.sendAnnouncement(tokenEntity.token, announcementToSend)
+            }
+
+        } catch (e: Exception) {
+            Log.e("Send Announcement", e.message ?: "")
+        }
 
     }
 
