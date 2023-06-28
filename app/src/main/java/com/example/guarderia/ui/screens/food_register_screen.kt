41a3f2b87d3915c1b2f0cec3157210b7bf4512c6
Samuel Bautista <samuelbaubas@gmail.com>
2023-06-28 10:34:22 -0600

    Add food screen

---
+++ app/src/main/java/com/example/guarderia/ui/screens/food_register_screen.kt
@@ -0,0 +1,10 @@
+package com.example.guarderia.ui.screens
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun FoodRegisterScreen(modifier: Modifier = Modifier) {
+    Text(text = "Holita")
+}
