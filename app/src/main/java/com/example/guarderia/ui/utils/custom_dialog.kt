b01b1a76350131481b6a9adedcb04420e3fa511c
Samuel Bautista <samuelbaubas@gmail.com>
2023-07-01 19:59:39 -0600

    use CustomDialog to update ingestion

--- app/src/main/java/com/example/guarderia/ui/utils/custom_dialog.kt
+++ app/src/main/java/com/example/guarderia/ui/utils/custom_dialog.kt
@@ -3,133 +3,95 @@ package com.example.guarderia.ui.utils
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.Face
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import com.example.guarderia.domain.entities.Food
-import com.example.guarderia.ui.theme.GeneralColor
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import com.example.guarderia.R
+import com.example.guarderia.domain.viewmodel.foodRegister.FoodRegisterViewModel
+import com.example.guarderia.model.Food
+import com.example.guarderia.model.Ingestion
+import kotlinx.coroutines.runBlocking
 
 
 @Composable
-fun CustomDialog(openDialog: MutableState<Boolean>) {
-    val selectedFood = remember { mutableStateOf("") }
-    val selectedCode = remember { mutableStateOf("") }
+fun CustomDialog(
+    modifier: Modifier = Modifier,
+    openDialog: MutableState<Boolean>,
+    ingestion: Ingestion,
+    food: Food,
+    foodRegisterViewModel: FoodRegisterViewModel,
+    type: String
+) {
+    val selectedOption = remember { mutableStateOf(ingestion.gratification) }
+
 
     if (openDialog.value) {
         AlertDialog(
             onDismissRequest = {
-                selectedFood.value = ""
-                selectedCode.value = ""
                 openDialog.value = false
             },
             title = {
-                Text(text = "Cual es las comida que desea agregar?")
+                Text(text = "${ingestion.firstSurname} ${ingestion.secondSurname} ${ingestion.name} ")
             },
             text = {
-                val expanded = remember { mutableStateOf(false) }
-                val expandedCodes = remember { mutableStateOf(false) }
+                val foodGratifications = listOf(
+                    stringResource(id = R.string.eatAll),
+                    stringResource(id = R.string.eatFew),
+                    stringResource(id = R.string.notEat),
+                    stringResource(id = R.string.absent),
+                )
                 Column() {
-
-                    TextButton(
-                        onClick = {
-                            expanded.value = !expanded.value
-                            expandedCodes.value = false
-                        },
-                        modifier = Modifier.fillMaxWidth(),
-                        colors = ButtonDefaults.buttonColors(
-                            backgroundColor = Color.LightGray,
-                        ),
-                    ) {
-                        if (selectedFood.value == "") {
-                            TypeDropMenu(type = "Comidas")
-                        } else {
-                            Text(text = selectedFood.value)
-                        }
-                    }
-                    DropdownMenu(
-                        expanded = expanded.value,
-                        onDismissRequest = { expanded.value = false },
-                    ) {
-                        DropdownMenuItem(onClick = {
-                            selectedFood.value = "Comida 1"
-                            expanded.value = false
-                        }) {
-                            Text("Comida 1")
-                        }
-                        DropdownMenuItem(onClick = {
-                            selectedFood.value = "Comida 2"
-                            expanded.value = false
-                        }) {
-                            Text("Comida 2")
+                    foodGratifications.forEachIndexed { index, gratification ->
+                        Row(
+                            modifier.fillMaxWidth(),
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.SpaceBetween
+                        ) {
+                            RadioButton(
+                                selected = index + 1 == selectedOption.value,
+                                onClick = { selectedOption.value = index + 1 })
+                            Text(text = gratification)
+                            IngestionIcon(gratification = index + 1) {
+                                selectedOption.value = index + 1
+                            }
                         }
                     }
 
-                    TextButton(
-                        onClick = {
-                            expandedCodes.value = !expandedCodes.value
-                            expanded.value = false
-                        },
-                        modifier = Modifier.fillMaxWidth(),
-                        colors = ButtonDefaults.buttonColors(
-                            backgroundColor = Color.LightGray,
-                        ),
-                    ) {
-                        if (selectedCode.value == "") {
-                            TypeDropMenu(type = "Status")
-                        } else {
-                            Text(text = selectedCode.value)
-                        }
-                    }
-                    DropdownMenu(
-                        expanded = expandedCodes.value,
-                        onDismissRequest = { expandedCodes.value = false },
-                    ) {
-                        DropdownMenuItem(onClick = {
-                            selectedCode.value = "CT (Comio Todo)"
-                            expandedCodes.value = false
-                        }) {
-                            Text("CT (Comio Todo)")
-                        }
-                        DropdownMenuItem(onClick = {
-                            selectedCode.value = "NT (No Termino)"
-                            expandedCodes.value = false
-                        }) {
-                            Text("NT (No Termino)")
-                        }
-                        DropdownMenuItem(onClick = {
-                            selectedCode.value = "NQN (No Quiso Nada)"
-                            expandedCodes.value = false
-                        }) {
-                            Text("NQN (No Quiso Nada)")
-                        }
-                    }
                 }
             },
             buttons = {
                 Row(
-                    modifier = Modifier.fillMaxWidth(),
+                    modifier = modifier.fillMaxWidth(),
                     horizontalArrangement = Arrangement.SpaceEvenly
                 ) {
+                    Button(
+                        colors = ButtonDefaults.buttonColors(
+                            backgroundColor = colorResource(id = R.color.green_button)
+                        ),
+                        onClick = {
+                            runBlocking {
+                                foodRegisterViewModel.recordIngestion(
+                                    ingestion,
+                                    selectedOption.value,
+                                    ingestion.childId,
+                                    food.id
+                                )
+                                foodRegisterViewModel.getIngestions(type)
+                                openDialog.value = false
+
+                            }
+                        }
+                    ) {
+                        Text("Aceptar")
+                    }
                     Button(colors = ButtonDefaults.buttonColors(
                         backgroundColor = Color.Red
                     ), onClick = { openDialog.value = false }) {
                         Text(text = "Cancelar")
-                    }
-                    Button(
-                        colors = ButtonDefaults.buttonColors(
-                            backgroundColor = GeneralColor
-                        ),
-                        enabled = (selectedCode.value != "" && selectedFood.value != ""),
-                        onClick = {
-                            val newFood = Food(selectedFood.value, selectedCode.value)
-
-                            selectedCode.value = ""
-                            selectedFood.value = ""
-                            openDialog.value = false
-                        }
-                    ) {
-                        Text("Aceptar")
                     }
                 }
             }
@@ -139,9 +101,22 @@ fun CustomDialog(openDialog: MutableState<Boolean>) {
 
 
 @Composable
-fun TypeDropMenu(type: String) {
-    Row {
-        Text(text = type)
-        Icon(Icons.Filled.ArrowDropDown, "Drop menu about $type")
+fun IngestionIcon(gratification: Int, onChangeSelection: () -> Unit) {
+    val backgroundColor = when (gratification) {
+        1 -> colorResource(id = R.color.green_button_disable)
+        2 -> colorResource(id = R.color.eatingFewColor)
+        3 -> colorResource(id = R.color.notEatingColor)
+        4 -> colorResource(id = R.color.absentColor)
+        else -> {
+            colorResource(id = R.color.unknown)
+        }
+    }
+    Button(
+        onClick = onChangeSelection, colors = ButtonDefaults.buttonColors(
+            backgroundColor = backgroundColor
+        )
+    ) {
+
+        Icon(Icons.Default.Face, contentDescription = null)
     }
 }
