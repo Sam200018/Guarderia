aac6fad289c72382fb7607df7f0cf76736e66561
Samuel Bautista <samuelbaubas@gmail.com>
2023-07-01 19:58:44 -0600

    update ingestion repository and data source remote

--- app/src/main/java/com/example/guarderia/data/IngestionsRepository.kt
+++ app/src/main/java/com/example/guarderia/data/IngestionsRepository.kt
@@ -1,11 +1,23 @@
 package com.example.guarderia.data
 
+import com.example.guarderia.model.IngestionRequest
 import com.example.guarderia.model.IngestionsRequest
 import com.example.guarderia.model.IngestionsResponse
 import com.example.guarderia.network.IngestionsDataSourceRemote
 
 interface IngestionsRepository {
-    suspend fun getIngestionsByGroup(token: String, ingestionsRequest: IngestionsRequest):IngestionsResponse
+    suspend fun getIngestionsByGroup(
+        token: String,
+        ingestionsRequest: IngestionsRequest
+    ): IngestionsResponse
+
+    suspend fun recordIngestion(token: String, ingestionRequest: IngestionRequest)
+
+    suspend fun editIngestion(
+        token: String,
+        ingestionId: String,
+        ingestionRequest: IngestionRequest
+    )
 }
 
 class IngestionsRepositoryImpl(
@@ -13,5 +25,18 @@ class IngestionsRepositoryImpl(
 ) : IngestionsRepository {
 
     override suspend fun getIngestionsByGroup(token: String, ingestionsRequest: IngestionsRequest) =
-        ingestionsDataSourceRemote.getIngestionsByGroup(token, ingestionsRequest.type,ingestionsRequest.date)
+        ingestionsDataSourceRemote.getIngestionsByGroup(
+            token,
+            ingestionsRequest.type,
+            ingestionsRequest.date
+        )
+
+    override suspend fun recordIngestion(token: String, ingestionRequest: IngestionRequest) =
+        ingestionsDataSourceRemote.createIngestion(token, ingestionRequest)
+
+    override suspend fun editIngestion(
+        token: String,
+        ingestionId: String,
+        ingestionRequest: IngestionRequest
+    ) = ingestionsDataSourceRemote.editIngestion(token, ingestionId, ingestionRequest)
 }
--- app/src/main/java/com/example/guarderia/network/IngestionsDataSource.kt
+++ app/src/main/java/com/example/guarderia/network/IngestionsDataSource.kt
@@ -1,8 +1,12 @@
 package com.example.guarderia.network
 
+import com.example.guarderia.model.IngestionRequest
 import com.example.guarderia.model.IngestionsResponse
+import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.POST
+import retrofit2.http.PUT
 import retrofit2.http.Path
 
 interface IngestionsDataSourceRemote {
@@ -13,4 +17,17 @@ interface IngestionsDataSourceRemote {
         @Path("type") type: String,
         @Path("date") date: String,
     ): IngestionsResponse
+
+    @POST("createIngestion")
+    suspend fun createIngestion(
+        @Header("Authorization") token: String,
+        @Body ingestionRequest: IngestionRequest
+    )
+
+    @PUT("editIngesta/{id_ingesta}")
+    suspend fun editIngestion(
+        @Header("Authorization") token: String,
+        @Path("id_ingesta") ingestionId: String,
+        @Body ingestionRequest: IngestionRequest
+    )
 }
